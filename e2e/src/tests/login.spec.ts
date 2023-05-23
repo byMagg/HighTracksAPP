3a2c4a4df5d799e8403617337113eb8d75a5a4b7
byMagg <byMag@hotmail.es>
2023-05-23 12:04:07 +0200

    remove firefox

--- e2e/src/tests/login.spec.ts
+++ e2e/src/tests/login.spec.ts
@@ -29,28 +29,3 @@ describe('Login Chrome', function () {
 })
 
 
-describe('Login Firefox', function () {
-    this.timeout(30000)
-    let driver: any;
-    let vars
-    beforeEach(async function () {
-        driver = await new Builder().forBrowser('firefox').build()
-        vars = {}
-    })
-    afterEach(async function () {
-        await driver.quit();
-    })
-    it('Login', async function () {
-        await driver.get("http://hightracks.me/")
-        await driver.manage().window().setRect({ width: 1358, height: 821 })
-        await driver.findElement(By.id("open-login")).click()
-        await driver.sleep(500)
-        await driver.findElement(By.id("ion-input-0")).click()
-        await driver.findElement(By.id("ion-input-0")).sendKeys("dani@gmail.com")
-        await driver.findElement(By.id("ion-input-1")).sendKeys("123456")
-        await driver.findElement(By.css(".button-solid")).click()
-        await driver.findElement(By.id("open-login")).click()
-        await driver.sleep(500)
-        await driver.findElement(By.css(".button-solid")).click()
-    })
-})
