f915e84736113e8c7d36881a584275f687b945c4
byMagg <byMag@hotmail.es>
2023-05-24 22:28:26 +0200

    update

--- src/app/home/recommendations/recommendations.page.spec.ts
+++ src/app/home/recommendations/recommendations.page.spec.ts
@@ -1,11 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RecommendationsPage } from './recommendations.page';
+import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
 
 describe('RecommendationsPage', () => {
   let component: RecommendationsPage;
   let fixture: ComponentFixture<RecommendationsPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, AngularFireModule.initializeApp(environment.firebase)],
+    }).compileComponents();
     fixture = TestBed.createComponent(RecommendationsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/home/tracks/track/track.page.spec.ts
+++ src/app/home/tracks/track/track.page.spec.ts
@@ -5,6 +5,9 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { Album } from 'src/app/models/album.model';
+import { Track } from 'src/app/models/track.model';
 
 
 describe('TrackPage', () => {
@@ -22,5 +25,18 @@ describe('TrackPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should define trackid correctly", () => {
+    const mockApiService: any = {
+      getTrack: function () {
+        return Promise.resolve(new Track("test", new Album("test", "test", "test")));
+      }
+    }
+    const mockActivatedRoute: any = {
+      params: of({ id: "123" })
+    }
+    const trackPage = new TrackPage(mockApiService, mockActivatedRoute);
+    expect(trackPage.trackId).toEqual("123");
   });
 });
